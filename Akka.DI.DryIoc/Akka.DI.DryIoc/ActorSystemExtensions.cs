cc4346a52d5a81ce07d5d946896e72445db287c2
Aleksey <aleks-herou@mail.ru>
2020-02-10 11:08:42 +0300

    Init

---
+++ Akka.DI.DryIoc/Akka.DI.DryIoc/ActorSystemExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Akka.DI.Core;
+using Akka.DI.DryIoc;
+
+using DryIoc;
+
+namespace Akka.Actor
+{
+    public static class ActorSystemExtensions
+    {
+        public static ActorSystem UseDryIoC(this ActorSystem system, IContainer container)
+            => system.UseDryIoC(container, out IDependencyResolver dependencyResolver);
+
+        public static ActorSystem UseDryIoC(this ActorSystem system, IContainer container, out IDependencyResolver dependencyResolver)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+
+            dependencyResolver = new DryIocDependencyResolver(container, system);
+
+            return system;
+        }
+    }
+}
---
+++ Akka.DI.DryIoc/Akka.DI.DryIoc/DryIocDependencyResolver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using Akka.Actor;
+using Akka.DI.Core;
+
+using DryIoc;
+
+namespace Akka.DI.DryIoc
+{
+    public sealed class DryIocDependencyResolver : IDependencyResolver, INoSerializationVerificationNeeded
+    {
+        private readonly IContainer container;
+        private readonly ConcurrentDictionary<string, Type> typeCache;
+        private readonly ActorSystem system;
+        private readonly ConditionalWeakTable<ActorBase, IResolverContext> references;
+
+        public DryIocDependencyResolver(IContainer container, ActorSystem system)
+        {
+            this.container = container
+                ?? throw new ArgumentNullException("container");
+
+            this.system = system
+                ?? throw new ArgumentNullException("system");
+
+            this.typeCache = new ConcurrentDictionary<string, Type>(StringComparer.OrdinalIgnoreCase);
+            this.references = new ConditionalWeakTable<ActorBase, IResolverContext>();
+
+            this.system.AddDependencyResolver(this);
+        }
+
+        public Func<ActorBase> CreateActorFactory(Type actorType)
+            => () =>
+            {
+                var context = this.container.OpenScope();
+                var key = (ActorBase)context.Resolve(actorType);
+                this.references.Add(key, context);
+                return key;
+            };
+
+        public Type GetType(string actorName)
+        {
+            var type = actorName.GetTypeValue();
+            if (type is null)
+                type = this.container.GetServiceRegistrations()
+                                     .Where(registration =>
+                                        registration.ImplementationType.Name.Equals(actorName, StringComparison.OrdinalIgnoreCase))
+                                     .Select(registration =>
+                                        registration.ImplementationType)
+                                     .FirstOrDefault();
+
+            this.typeCache.TryAdd(actorName, type);
+
+            return this.typeCache[actorName];
+        }
+
+        public Props Create<TActor>()
+            where TActor : ActorBase
+            => this.Create(typeof(TActor));
+
+        public Props Create(Type actorType)
+            => this.system.GetExtension<DIExt>().Props(actorType);
+
+        public void Release(ActorBase actor)
+        {
+            if (!this.references.TryGetValue(actor, out IResolverContext lifetimeScope))
+                return;
+
+            lifetimeScope.Dispose();
+            this.references.Remove(actor);
+        }
+    }
+}
